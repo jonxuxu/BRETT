7095376091ab47e304e63ca06683482b582833b8
Jason Guo <jason.guo1@uwaterloo.ca>
2019-11-21 16:54:42 -0500

    add greyscale and greying of images

--- image-stitching/image-stitching.py
+++ image-stitching/image-stitching.py
@@ -5,17 +5,9 @@ import argparse
 import imutils
 import cv2
 
-# Create argument parser, then parse arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", type=str, required=True,
-                help="path to input directory of images to stitch")
-ap.add_argument("-o", "--output", type=str, required=True,
-                help="path to the output image")
-args = vars(ap.parse_args())
-
 # grab the paths to the input images and initialize our images list
 print("Loading images...")
-imagePaths = sorted(list(paths.list_images(args["images"])))
+imagePaths = sorted(list(paths.list_images("images")))
 images = []
 
 # loop over the image paths, load each one, and add them to our
@@ -33,12 +25,22 @@ stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
 # if the status is '0', then OpenCV successfully performed image
 # stitching
 if status == 0:
+    # Crop black bezel out of image
+    print("Croping images...")
+    stitched = cv2.copyMakeBorder(stitched, 10, 10, 10, 10,
+                                  cv2.BORDER_CONSTANT, (0, 0, 0))
+
+    # convert image to greyscale, then set foreground to be
+    # white (255), while background to be black (0)
+    gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
+
     # write the output stitched image to disk
-    cv2.imwrite(args["output"], stitched)
+    cv2.imwrite("output.png", stitched)
 
     # display the output stitched image to our screen
-    cv2.imshow("Stitched", stitched)
-    # cv2.waitKey(0)
+    cv2.imshow("Stitched", thresh)
+    cv2.waitKey(0)
 
 # otherwise the stitching failed, likely due to not enough keypoints)
 # being detected
