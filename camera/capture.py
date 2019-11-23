4c4830069b34cf5aae011695a7a9966e9bedbe35
Jonathan <jonathanxu25@gmail.com>
2019-11-23 18:34:37 -0500

    data saved in folders

--- camera/capture.py
+++ camera/capture.py
@@ -1,22 +1,27 @@
 from time import sleep
 from picamera import PiCamera
 import calendar
+import os
 
+# Camera setup
 camera = PiCamera()
 camera.resolution = (2592, 1944)  # Resolution of pi camera 1.3
 camera.start_preview()
 
 # Create new log file
 count = 1
-while(!path.exists("log" + str(count) + ".txt")){
+while(not path.exists(str(count))){
     count += 1
 }
-f = open("log" + str(count) + ".txt", "w+")
+os.mkdir(str(count))
+f = open(str(count) + "/log.txt", "w+")
 f.write("Timestamp:\n")
 
 # Camera warm-up time
 sleep(2)
-for filename in camera.capture_continuous('{counter:03d}.jpg'):
+for filename in camera.capture_continuous(str(count) + '/{counter:03d}.jpg'):
     f.write(calendar.timegm(dt.utctimetuple() + "\n")
     print('Captured %s' % filename)
     sleep(5)  # wait 5 seconds
+
+f.close()
