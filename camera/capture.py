31053420498084e16874c3ce1fac85a1f994191b
Jonathan <jonathanxu25@gmail.com>
2019-11-23 18:25:10 -0500

    datetime logging

--- camera/capture.py
+++ camera/capture.py
@@ -1,11 +1,22 @@
 from time import sleep
 from picamera import PiCamera
+import calendar
 
 camera = PiCamera()
 camera.resolution = (2592, 1944)  # Resolution of pi camera 1.3
 camera.start_preview()
+
+# Create new log file
+count = 1
+while(!path.exists("log" + str(count) + ".txt")){
+    count += 1
+}
+f = open("log" + str(count) + ".txt", "w+")
+f.write("Timestamp:\n")
+
 # Camera warm-up time
 sleep(2)
-for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+for filename in camera.capture_continuous('{counter:03d}.jpg'):
+    f.write(calendar.timegm(dt.utctimetuple() + "\n")
     print('Captured %s' % filename)
     sleep(5)  # wait 5 seconds
