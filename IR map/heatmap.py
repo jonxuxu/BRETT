785598475ac377339d51f3766f0c32d2bc166c32
Joshua Cheng <j885cheng@uwaterloo.ca>
2019-11-23 23:48:38 -0500

    added fileIn and heatmap creation functionality

--- IR map/heatmap.py
+++ IR map/heatmap.py
@@ -1,19 +1,32 @@
 import numpy as np
 import numpy.random
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
-# Create data
-x = np.random.randn(4096)
-y = np.random.randn(4096)
+# File I/O
+with open('out.txt', encoding='utf-8') as file:
+    posx = []
+    posy = []
+    temp = []
+    file.readline()
+    for line in file:
+        line = line.strip().split(',')
+        posx.append(float(line[1]))
+        posy.append(float(line[2]))
+        temp.append(float(line[4]) - float(line[3]))
 
-# Create heatmap
-heatmap, xedges, yedges = np.histogram2d(x, y, bins=(64, 64))
-extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-
-# Plot heatmap
-plt.clf()
-plt.title("Heatmap example")
-plt.ylabel("y")
-plt.xlabel("x")
-plt.imshow(heatmap, extent=extent)  # origin="lower" to flip it
+# Create heat map
+reds = sns.set_palette(sns.color_palette("coolwarm", 25))  # turn down the intensity
+df = pd.DataFrame({'PositionX': posx, 'PositionY': posy, 'Temperature': temp})
+data = df.pivot(index="PositionX", columns="PositionY", values="Temperature")
+# heatMap = sns.heatmap(data, annot=True, cmap="coolwarm", center=1)  # change center value to change color scale
+heatMap = sns.heatmap(data, cmap='coolwarm', center=1, yticklabels=False, xticklabels=False, cbar=False)
+heatMap.invert_yaxis()
+heatMap.set_ylabel('')
+heatMap.set_xlabel('')
 plt.show()
+
+# Sorted data
+print(df)
+print(data)
