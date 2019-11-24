841a22a06ad29c5f54aa7696ade97bae116f30d4
Jonathan <jonathanxu25@gmail.com>
2019-11-23 19:54:49 -0500

    breaking function

--- camera/capture.py
+++ camera/capture.py
@@ -15,32 +15,40 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(21, GPIO.OUT)
 GPIO.output(21, 1)
-while(not GPIO.input(16)):
-    if(GPIO.input(20)):  # Connect BCM 20 to 21 to shutdown
-        break
+
+
+def main():
     while(not GPIO.input(16)):
-        print("Pin disconnected. Connect BCM pins 16 and 21 to start capture. Disconnect pins to stop.")
-        time.sleep(1)
-    print("Starting capture.")
+        while(not GPIO.input(16)):
+            if(GPIO.input(20)):  # Connect BCM 20 to 21 to shutdown
+                return
+            print(
+                "Pin disconnected. Connect BCM pins 16 and 21 to start capture. Disconnect pins to stop.")
+            time.sleep(1)
+        print("Starting capture.")
 
-    # Create new log file
-    count = 1
-    while(os.path.exists(str(count))):
-        count += 1
+        # Create new log file
+        count = 1
+        while(os.path.exists(str(count))):
+            count += 1
 
-    os.mkdir(str(count))
-    f = open(str(count) + "/log.txt", "w+")
-    f.write("Timestamp: " + str(time.time()) + "\n")
+        os.mkdir(str(count))
+        f = open(str(count) + "/log.txt", "w+")
+        f.write("Timestamp: " + str(time.time()) + "\n")
 
-    # Camera warm-up time
-    for filename in camera.capture_continuous(str(count) + "/{counter:03d}.jpg"):
-        f.write(str(time.time()) + "\n")
-        print('Captured %s' % filename)
-        if(not GPIO.input(16)):
-            print("Pin disconnected. Ending capture.")
-            break
-        time.sleep(5)  # wait 5 seconds
-    f.close()
+        # Camera warm-up time
+        for filename in camera.capture_continuous(str(count) + "/{counter:03d}.jpg"):
+            f.write(str(time.time()) + "\n")
+            print('Captured %s' % filename)
+            if(not GPIO.input(16)):
+                print("Pin disconnected. Ending capture.")
+                break
+            time.sleep(5)  # wait 5 seconds
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
 
 while(True):
     if(GPIO.input(20)):
