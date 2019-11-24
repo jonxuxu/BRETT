16a7d246ff6a7cc69b22866bef1607ed1383df9c
Jonathan <jonathanxu25@gmail.com>
2019-11-23 19:03:12 -0500

    auto start pins

--- camera/capture.py
+++ camera/capture.py
@@ -1,11 +1,20 @@
 import time
 from picamera import PiCamera
 import os
+import RPi.GPIO as GPIO
 
 # Camera setup
 camera = PiCamera()
 camera.resolution = (2592, 1944)  # Resolution of pi camera 1.3
 camera.start_preview()
+
+# Wait until pins 4(5V) and 8(GPIO14) are connected
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.IN)
+print("Pin disconnected. Connect pin 4 to 8 to start capture. Disconnect pins to stop.")
+while(not GPIO.input(0)):
+    time.sleep(1)
+print("Starting capture.")
 
 # Create new log file
 count = 1
@@ -17,10 +26,11 @@ f = open(str(count) + "/log.txt", "w+")
 f.write("Timestamp: " + str(time.time()) + "\n")
 
 # Camera warm-up time
-time.sleep(1)
 for filename in camera.capture_continuous(str(count) + "/{counter:03d}.jpg"):
     f.write(str(time.time()) + "\n")
     print('Captured %s' % filename)
+    if(not GPIO.input(0)):
+        print("Pin disconnected. Ending capture.")
     time.sleep(5)  # wait 5 seconds
 
 f.close()
