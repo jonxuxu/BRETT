a84c757e3beac2c02d08e3109fa969db3458c97d
Joshua Cheng <j885cheng@uwaterloo.ca>
2019-11-25 18:49:34 -0500

    fixed heatmap by replacing it with a kdeplot

--- IR map/heatmap.py
+++ IR map/heatmap.py
@@ -2,36 +2,33 @@ import numpy as np
 import numpy.random
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
+import math
 
 # File I/O
 with open('test_flight_3.txt', encoding='utf-8') as file:
     posx = []
     posy = []
-    temp = []
     file.readline()
     for line in file:
         line = line.strip().split(',')
-        posx.append(float(line[1]))
-        posy.append(float(line[2]))
-        temp.append(float(line[6]) - float(line[5]))
+        x = line[1]
+        y = line[2]
+        temp = math.floor(float(line[6]) - float(line[5]))
+        if temp > 0:
+            for i in range(0, temp):
+                posx.append(x)
+                posy.append(y)
+        else:
+            posx.append(x)
+            posy.append(y)
 
 # Create heat map
 reds = sns.set_palette(sns.color_palette("coolwarm", 25))  # turn down the intensity
-df = pd.DataFrame({'PositionX': posx, 'PositionY': posy, 'Temperature': temp})
-data = df.pivot(index="PositionX", columns="PositionY", values="Temperature")
-# heatMap = sns.heatmap(data, annot=True, cmap="coolwarm", center=1)  # change center value to change color scale
+datax = np.array(posx)
+datay = np.array(posy)
 plt.figure(figsize=(95.28, 47.56))
-heatMap = sns.heatmap(data, cmap='coolwarm', center=1, yticklabels=False, xticklabels=False, cbar=False)
-heatMap.invert_yaxis()
-heatMap.set_ylabel('')
-heatMap.set_xlabel('')
-
-# Save a png of the created heatmap
+heatMap = sns.kdeplot(datax, datay, n_levels=50, shade=True, cmap='Reds', shade_lowest=False)
+plt.show()
 fig = heatMap.get_figure()
 fig.savefig("heatmap.png")
 
-# Printing out the created maps and tables
-plt.show()
-print(df)
-print(data)
